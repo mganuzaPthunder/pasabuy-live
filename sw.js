ec7dc7979dfc4ddb388de8180392025da0a0c8d4
myleen luna <myleen@myleens-MacBook-Pro.local>
2026-07-28 14:00:08 +0800

    Fix two-column Live layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pasabuy-v3';
+const CACHE = 'pasabuy-v4';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
